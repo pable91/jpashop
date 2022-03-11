c7d373e22e13bfe65187cb2a830dab5ef2a8955c
KimYongKwon <dydrnjs518@gmail.com>
2022-03-11 22:19:03 +0900

    상품 도메인 개발

---
+++ src/main/java/jpabook/jpashop/service/MemberService.java
@@ -0,0 +1,43 @@
+package jpabook.jpashop.service;
+
+import jpabook.jpashop.domain.Member;
+import jpabook.jpashop.repository.MemberRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+@Transactional(readOnly = true)
+@RequiredArgsConstructor
+public class MemberService {
+
+    private final MemberRepository memberRepository;
+
+    // 회원 가입
+    @Transactional
+    public Long join(Member member) {
+        validateDuplicateMember(member);
+        memberRepository.save(member);
+        // save 메소드 호출 후 영속상태가 되면서 1차 캐시에 저장되고 그때 key값(id)을
+        // Member객체 id도 채워준다.(DB에 적용이 안되어도 여기까지 완료됨)
+        return member.getId();
+    }
+
+    private void validateDuplicateMember(Member member) {
+        List<Member> findMembers = memberRepository.findByName(member.getName());
+        if (!findMembers.isEmpty()) {
+            throw new IllegalStateException("이미 존재하는 회원입니다.");
+        }
+    }
+
+    // 회원 전체 조회
+    private List<Member> findMember() {
+        return memberRepository.findAll();
+    }
+
+    public Member findOne(Long memberId) {
+        return memberRepository.findOne(memberId);
+    }
+}
